e99ae7808ef2b6ee3d09b75a670e12fb8f290335
Kinan Mohrez <kinanmohrez2015@gmail.com>
2022-06-09 15:52:27 +0200

    model added

---
+++ cookbook.js
@@ -0,0 +1,22 @@
+function toggle(kinan) {
+
+
+
+  var x = document.getElementById(kinan);
+
+
+  if (x.style.display === "none") {
+
+
+    x.style.display = "block";
+
+
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+const text = document.querySelector('#result');
+text.innerHTML = "<p>kinan melis christoph jh jhgvkjfiykgfaksjou</p>"
